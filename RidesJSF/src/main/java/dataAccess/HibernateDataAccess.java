a7e1377d091295df7591ced8742b3f07913d611d
Usuario <Usuario@DESKTOP-C3ESJLV>
2025-12-02 11:49:38 +0100

    hibernate medio hecho

---
+++ RidesJSF/src/main/java/dataAccess/HibernateDataAccess.java
@@ -0,0 +1,223 @@
+package dataAccess;
+
+import java.io.File;
+import java.net.NoRouteToHostException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.ResourceBundle;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.Persistence;
+import javax.persistence.TypedQuery;
+
+import configuration.UtilDate;
+
+import domain.Driver;
+import domain.Ride;
+import eredua.JPAUtil;
+import exceptions.RideAlreadyExistException;
+import exceptions.RideMustBeLaterThanTodayException;
+
+/**
+ * It implements the data access to the objectDb database
+ */
+public class HibernateDataAccess  {
+	private  EntityManager  db;
+
+     public HibernateDataAccess()  {
+    	 db = JPAUtil.getEntityManager();
+//		if (c.isDatabaseInitialized()) {
+//			String fileName=c.getDbFilename();
+//
+//			File fileToDelete= new File(fileName);
+//			if(fileToDelete.delete()){
+//				File fileToDeleteTemp= new File(fileName+"$");
+//				fileToDeleteTemp.delete();
+//
+//				  System.out.println("File deleted");
+//				} else {
+//				  System.out.println("Operation failed");
+//				}
+//		}
+//		open();
+//		if  (c.isDatabaseInitialized())initializeDB();
+//		
+//		System.out.println("DataAccess created => isDatabaseLocal: "+c.isDatabaseLocal()+" isDatabaseInitialized: "+c.isDatabaseInitialized());
+//
+//		close();
+
+	}
+     
+    public HibernateDataAccess(EntityManager db) {
+    	this.db=db;
+    }
+
+	
+	
+	/**
+	 * This is the data access method that initializes the database with some events and questions.
+	 * This method is invoked by the business logic (constructor of BLFacadeImplementation) when the option "initialize" is declared in the tag dataBaseOpenMode of resources/config.xml file
+	 */	
+	public void initializeDB(){
+		
+		db.getTransaction().begin();
+
+		try {
+
+		   Calendar today = Calendar.getInstance();
+		   
+		   int month=today.get(Calendar.MONTH);
+		   int year=today.get(Calendar.YEAR);
+		   if (month==12) { month=1; year+=1;}  
+	    
+		   
+		    //Create drivers 
+			Driver driver1=new Driver("driver1@gmail.com","Aitor Fernandez");
+			Driver driver2=new Driver("driver2@gmail.com","Ane Gaztañaga");
+			Driver driver3=new Driver("driver3@gmail.com","Test driver");
+
+			
+			//Create rides
+			
+						
+			db.persist(driver1);
+			db.persist(driver2);
+			db.persist(driver3);
+
+	
+			db.getTransaction().commit();
+			System.out.println("Db initialized");
+		}
+		catch (Exception e){
+			e.printStackTrace();
+		}
+	}
+	
+	/**
+	 * This method returns all the cities where rides depart 
+	 * @return collection of cities
+	 */
+	public List<String> getDepartCities(){
+			TypedQuery<String> query = db.createQuery("SELECT DISTINCT r.from FROM Ride r ORDER BY r.from", String.class);
+			List<String> cities = query.getResultList();
+			return cities;
+		
+	}
+	/**
+	 * This method returns all the arrival destinations, from all rides that depart from a given city  
+	 * 
+	 * @param from the depart location of a ride
+	 * @return all the arrival destinations
+	 */
+	public List<String> getArrivalCities(String from){
+		TypedQuery<String> query = db.createQuery("SELECT DISTINCT r.to FROM Ride r WHERE r.from=?1 ORDER BY r.to",String.class);
+		query.setParameter(1, from);
+		List<String> arrivingCities = query.getResultList(); 
+		return arrivingCities;
+		
+	}
+	/**
+	 * This method creates a ride for a driver
+	 * 
+	 * @param from the origin location of a ride
+	 * @param to the destination location of a ride
+	 * @param date the date of the ride 
+	 * @param nPlaces available seats
+	 * @param driverEmail to which ride is added
+	 * 
+	 * @return the created ride, or null, or an exception
+	 * @throws RideMustBeLaterThanTodayException if the ride date is before today 
+ 	 * @throws RideAlreadyExistException if the same ride already exists for the driver
+	 */
+	public Ride createRide(String from, String to, Date date, int nPlaces, float price, String driverEmail) throws  RideAlreadyExistException, RideMustBeLaterThanTodayException {
+		System.out.println(">> DataAccess: createRide=> from= "+from+" to= "+to+" driver="+driverEmail+" date "+date);
+		try {
+			if(new Date().compareTo(date)>0) {
+				throw new RideMustBeLaterThanTodayException(ResourceBundle.getBundle("Etiquetas").getString("CreateRideGUI.ErrorRideMustBeLaterThanToday"));
+			}
+			db.getTransaction().begin();
+			
+			Driver driver = db.find(Driver.class, driverEmail);
+			if (driver.doesRideExists(from, to, date)) {
+				db.getTransaction().commit();
+				throw new RideAlreadyExistException(ResourceBundle.getBundle("Etiquetas").getString("DataAccess.RideAlreadyExist"));
+			}
+			System.out.println("crea addRide");
+			Ride ride = driver.addRide(from, to, date, nPlaces, price);
+			//next instruction can be obviated
+			db.persist(driver); 
+			db.getTransaction().commit();
+
+			return ride;
+		} catch (NullPointerException e) {
+			// TODO Auto-generated catch block
+			db.getTransaction().commit();
+			return null;
+		}
+		
+		
+	}
+	
+	/**
+	 * This method retrieves the rides from two locations on a given date 
+	 * 
+	 * @param from the origin location of a ride
+	 * @param to the destination location of a ride
+	 * @param date the date of the ride 
+	 * @return collection of rides
+	 */
+	public List<Ride> getRides(String from, String to, Date date) {
+		System.out.println(">> DataAccess: getRides=> from= "+from+" to= "+to+" date "+date);
+
+		List<Ride> res = new ArrayList<Ride>();	
+		TypedQuery<Ride> query = db.createQuery("SELECT r FROM Ride r WHERE r.from=?1 AND r.to=?2 AND r.date=?3",Ride.class);   
+		query.setParameter(1, from);
+		query.setParameter(2, to);
+		query.setParameter(3, date);
+		List<Ride> rides = query.getResultList();
+	 	 for (Ride ride:rides){
+		   res.add(ride);
+		  }
+	 	return res;
+	}
+	
+	/**
+	 * This method retrieves from the database the dates a month for which there are events
+	 * @param from the origin location of a ride
+	 * @param to the destination location of a ride 
+	 * @param date of the month for which days with rides want to be retrieved 
+	 * @return collection of rides
+	 */
+	public List<Date> getThisMonthDatesWithRides(String from, String to, Date date) {
+		System.out.println(">> DataAccess: getEventsMonth");
+		List<Date> res = new ArrayList<Date>();	
+		
+		Date firstDayMonthDate= UtilDate.firstDayMonth(date);
+		Date lastDayMonthDate= UtilDate.lastDayMonth(date);
+				
+		
+		TypedQuery<Date> query = db.createQuery("SELECT DISTINCT r.date FROM Ride r WHERE r.from=?1 AND r.to=?2 AND r.date BETWEEN ?3 and ?4",Date.class);   
+		
+		query.setParameter(1, from);
+		query.setParameter(2, to);
+		query.setParameter(3, firstDayMonthDate);
+		query.setParameter(4, lastDayMonthDate);
+		List<Date> dates = query.getResultList();
+	 	 for (Date d:dates){
+		   res.add(d);
+		  }
+	 	return res;
+	}
+	
+	public void close(){
+		db.close();
+		System.out.println("DataAcess closed");
+	}
+	
+}
